0be1552f3bb2abfd8297e1878b19cc725a0720fe
LSPGLHB <231776007@qq.com>
2023-04-24 22:39:46 +0800

    v2023.4.24

--- panorama/scripts/custom_game/ui_banner_msg.js
+++ panorama/scripts/custom_game/ui_banner_msg.js
@@ -1,16 +1,19 @@
 GameEvents.Subscribe( "getTimeCountLUATOJS", getTimeCountLUATOJS);
 
 function getTimeCountLUATOJS(data){
-    var round = data.round
-    var step=data.step
-    var stepTime=data.stepTime
-    var gameTime=data.gameTime
-    var stepText = "第"+round+"轮战斗"
-    var timeText = step + stepTime + "秒"
-    $("#stepCountValue").text = stepText
-    $("#timeCountValue").text = timeText
-    $.Msg("gameTime:"+gameTime)
-    $.Msg("step:"+step+"stepTime:"+stepTime)
-   // $("#timeCountValue")
+    //var round = data.round
+    //var step=data.step
+    //var stepTime=data.stepTime
+    //var gameTime=data.gameTime
+
+    //var stepText = "第"+round+"轮战斗"
+    //var timeText = step + stepTime + "秒"
+
+    var topTips = data.topTips
+    var bottomTips = data.bottomTips
+   
+    $("#roundCountValue").text = topTips//stepText
+    $("#timeCountValue").text = bottomTips//timeText
+
 }
 
